c234b01e0127e8a3bc91af2fb0e581ffc9be690c
valechat443 <127490359+valechat443@users.noreply.github.com>
2023-12-26 10:45:37 +0100

    response create

---
+++ src/main/java/volley_net/volley_net/payload/response/PeriodsTeamsGameSpecific.java
@@ -0,0 +1,18 @@
+package volley_net.volley_net.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PeriodsTeamsGameSpecific {
+    private int first;
+    private int second;
+    private int third;
+    private int fourth;
+    private int fifth;
+}
---
+++ src/main/java/volley_net/volley_net/payload/response/TeamsGameSpecific.java
@@ -0,0 +1,22 @@
+package volley_net.volley_net.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamsGameSpecific {
+    private int id_team;
+    private String name;
+    private String logo;
+    private boolean national;
+    private boolean home;
+    private int set;
+    private List<PeriodsTeamsGameSpecific> periods;
+}

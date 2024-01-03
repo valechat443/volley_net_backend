2eba03d751a79d99baa24593aaf04fd0bea0a22c
Fra226 <127490567+Fra226@users.noreply.github.com>
2024-01-03 16:22:36 +0100

    Delete volley_net_backend-main directory

--- volley_net_backend-main/src/main/java/volley_net/volley_net/payload/request/SeasonIdLeague.java
+++
@@ -1,9 +0,0 @@
-package volley_net.volley_net.payload.request;
-
-import lombok.Getter;
-
-@Getter
-public class SeasonIdLeague {
-    private int season;
-    private int id_league;
-}
--- volley_net_backend-main/src/main/java/volley_net/volley_net/payload/request/StatisticRequest.java
+++
@@ -1,8 +0,0 @@
-package volley_net.volley_net.payload.request;
-
-import lombok.Getter;
-
-@Getter
-public class StatisticRequest {
-    private int id_team;
-}

fa8a5e2c387f6f5de053b5e748760dda49fc64b0
FrancescoBechini <francesco.bechinifb@gmail.com>
2024-01-15 15:47:49 +0100

    service schedule updateGames

--- src/main/java/volley_net/volley_net/service/SheduleService.java
+++ src/main/java/volley_net/volley_net/service/SheduleService.java
@@ -29,6 +29,9 @@ public class SheduleService {
 
     private final LeagueRepository leagueRepository;
 
+    private final JsonService jsonService;
+
+
     /**
      *controllo se un utente ha vinto una sua bet di un determinato game
      *
@@ -59,58 +62,62 @@ public class SheduleService {
      *aggiorna gli esiti e i punteggi di tutte le partite finite
      * !! Da TESTARE !!
      */
-    //@Scheduled(cron = "0 49 14 * * ?")
+    @Scheduled(cron = "0 40 15 * * ?")
     public void update_games() {
         try{
-            RestTemplate restTemplate = new RestTemplate();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.add("x-rapidapi-key", "d6f6c982d794f76eb4036f26b8387da0");
-            headers.add("x-rapidapi-host", "v1.volleyball.api-sports.io");
-
-            HttpEntity<String> entity = new HttpEntity<String>(headers);
-
-            ResponseEntity<String> cose = restTemplate.exchange(
-                    "https://v1.volleyball.api-sports.io/games?date="+LocalDate.now().minusDays(1).toString(),
-                    HttpMethod.GET,
-                    entity,
-                    String.class);
-
-            JSONObject j = new JSONObject(cose.getBody());
+            JSONObject j = jsonService.chiamata("https://v1.volleyball.api-sports.io/games?date="+LocalDate.now().minusDays(1).toString());
             JSONArray response = j.getJSONArray("response");
 
-            for (int i = 0; i < response.length(); i++) {
-                JSONObject game = response.getJSONObject(i);
-                if(game.getJSONObject("country").getString("code").equals("IT")){
-                    log.info(game.toString());
+            if(response.length()<1) {
+                log.info("Lunghezza response minore 1");
+            } else {
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject game = response.getJSONObject(i);
+                    if (game.getJSONObject("country").getString("code").equals("IT")) {
+                        Thread.sleep(10000);
+                        log.info(game.toString());
 
-                    //es. input: "2024-01-08T16:00:00+00:00"
-                    //Inizializzo la data senza il time
-                    String dateStr = game.getString("date").split("T")[0]; // Removes the time part
-                    LocalDate date = LocalDate.parse(dateStr);
-                    //Inizializzo il time
-                    String timeStr = game.getString("date").split("T")[1].split("\\+")[0];
-                    LocalTime time = LocalTime.parse(timeStr);
+                        //es. input: "2024-01-08T16:00:00+00:00"
+                        //Inizializzo la data senza il time
+                        log.info("date");
+                        String dateStr = game.getString("date").split("T")[0]; // Removes the time part
+                        LocalDate date = LocalDate.parse(dateStr);
+                        //Inizializzo il time
+                        log.info("time");
+                        String timeStr = game.getString("date").split("T")[1].split("\\+")[0];
+                        LocalTime time = LocalTime.parse(timeStr);
 
-                    //faccio un'altra chiamata (a odds)... finché non c'è un service che cerca oddsByIdGame... se ci sara
-                    ResponseEntity<String> oddsRp = restTemplate.exchange(
-                            "https://v1.volleyball.api-sports.io/odds?game="+game.getInt("id"),
-                            HttpMethod.GET,
-                            entity,
-                            String.class);
-                    JSONObject jo = new JSONObject(oddsRp.getBody());
-                    JSONObject bookmaker = jo.getJSONObject("response").getJSONArray("bookmakers").getJSONObject(0);
-                    JSONObject bet = bookmaker.getJSONArray("bets").getJSONObject(0);
-                    log.info(bet.getString("name"));
-                    Float oddsHome = Float.parseFloat(bet.getJSONArray("values").getJSONObject(0).getString("value"));
-                    Float oddsAway = Float.parseFloat(bet.getJSONArray("values").getJSONObject(1).getString("value"));
 
-                    gameRepository.save(new Game(game.getInt("id"), new League(game.getJSONObject("league").getInt("id")), date, time, game.getString("timezone"), game.getJSONObject("status").getString("long"), oddsHome, oddsAway, game.getString("week")));
+                        //faccio un'altra chiamata (a odds)... finché non c'è un service che cerca oddsByIdGame... se ci sara
+                        JSONObject jo = jsonService.chiamata("https://v1.volleyball.api-sports.io/odds?game=" + game.getInt("id"));
+                        JSONArray responseJo = jo.getJSONArray("response");
+                        log.info("chiamata odds");
+                        Float oddsHome = 1.0f;
+                        Float oddsAway = 1.0f;
+
+                        log.info("Game: "+ game.toString());
+                        log.info("Odds: "+ jo.toString());
+
+                        if(responseJo.length()>0){
+                            log.info("entro if odds");
+                            JSONObject bookmaker = responseJo.getJSONObject(0).getJSONArray("bookmakers").getJSONObject(0);
+                            log.info("bookmaker");
+                            JSONObject bet = bookmaker.getJSONArray("bets").getJSONObject(0);
+                            log.info("bet");
+                            oddsHome = Float.parseFloat(bet.getJSONArray("values").getJSONObject(0).getString("odd"));
+                            log.info("home");
+                            oddsAway = Float.parseFloat(bet.getJSONArray("values").getJSONObject(1).getString("odd"));
+                            log.info("away");
+                        }
+
+                        gameRepository.save(new Game(game.getInt("id"), new League(game.getJSONObject("league").getInt("id")), date, time, game.getString("timezone"), game.getJSONObject("status").getString("long"), oddsHome, oddsAway, game.getString("week")));
+
+                    }
                 }
             }
-        }catch (JSONException e) {
-            throw new RuntimeException(e);
+        }catch (Exception e) {
+          log.info( e.getMessage());
         }
     }
 
